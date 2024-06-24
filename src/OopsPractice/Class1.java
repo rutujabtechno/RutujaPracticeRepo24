aa09a17c15e890c63d5b1fb5e76b3f3c4b074b1b
Rutuja Bhagwat <rutujabhagvat7@gmail.com>
2024-06-24 16:36:35 +0530

    RutujaB_2024

--- src/OopsPractice/Class1.java
+++ src/OopsPractice/Class1.java
@@ -1,5 +1,16 @@
 package OopsPractice;
 
-public class Class1 {
-
+public class class1 {
+	
+	void printNumber(int startRange ,int endRange ) {
+		for(int i = startRange; i <= endRange; i++) {
+			if(i%5 == 0 && i%3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		class1 a = new class1();
+		a.printNumber(5,40);
+    }
 }
